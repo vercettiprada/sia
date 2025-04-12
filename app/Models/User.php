c393755ee8008a05fc37fc405cb48b498954fac0
vercettiprada <xaviersaligumba@gmail.com>
2025-04-12 19:09:43 +0800

    fuck

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements JWTSubject
+class User extends Model implements AuthenticatableContract, JWTSubject
 {
+    use Authenticatable;
+
     protected $table = 'tbl_user';
     protected $primaryKey = 'userid';
     public $timestamps = false;
